8325e11e071bbb7da53d47ae7c0cba318e3a279a
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-11-29 15:25:27 +0100

    adding the .h file

--- binary_trees.h
+++ binary_trees.h
@@ -47,13 +47,12 @@ int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
-void binary_tree_levelorder_util(const binary_tree_t *tree, void (*func)(int), size_t level);
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
+void binary_tree_levelorder_util(const binary_tree_t *tree,
+	void (*func)(int), size_t level);
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+	const binary_tree_t *second);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
-// size_t binary_tree_height(const binary_tree_t *tree)
-// void binary_tree_levelorder_util(const binary_tree_t *tree, void (*func)(int), size_t level)
 int binary_tree_is_bst(const binary_tree_t *tree);
-int is_bst_util(const binary_tree_t *node, int min, int max);
 
 #endif /* _BINARY_TREES_H_ */
