8c6872bb4361f6b8d1150b7fee5f05d56cc292cd
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-11-29 14:31:57 +0100

    some fixes

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -2,25 +2,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 #define MAX_QUEUE_SIZE 1024
-/**
- * binary_tree_height - Measures the height of a binary tree
- * @tree: Pointer to the root node of the tree to measure
- *
- * Return: Height of binary tree, 0 if tree is NULL
- */
-
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-	size_t l_h, r_h;
-
-	if (tree)
-	{
-		l_h = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-		r_h = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-		return ((l_h > r_h) ? l_h : r_h);
-	}
-	return (0);
-}
 
 /**
  * binary_tree_levelorder - Traverses binary tree using level-order traversal.
@@ -28,32 +9,28 @@ size_t binary_tree_height(const binary_tree_t *tree)
  * @tree: pointer to root node of tree to traverse.
  * @func: A pointer to a function to call for each node.
  */
-
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree == NULL || func == NULL)
-		return;
+    int front = 0, rear = 0;
+    const binary_tree_t *c;
+    const binary_tree_t **queue = malloc(sizeof(const binary_tree_t *) * MAX_QUEUE_SIZE);
 
-	binary_tree_t **queue = malloc(sizeof(binary_tree_t *)
-			* MAX_QUEUE_SIZE);
-	if (queue == NULL)
-		return;
+    if (tree == NULL || func == NULL || queue == NULL)
+        return;
 
-	int front = 0, rear = 0;
-	binary_tree_t *c;
+    queue[rear++] = tree;
 
-	queue[rear++] = (binary_tree_t *)tree;
+    while (front < rear)
+    {
+        c = queue[front++];
 
-	while (front < rear)
-	{
-		c = queue[front++];
+        func(c->n);
 
-		func(c->n);
+        if (c->left)
+            queue[rear++] = c->left;
+        if (c->right)
+            queue[rear++] = c->right;
+    }
 
-		if (c->left)
-			queue[rear++] = c->left;
-		if (c->right)
-			queue[rear++] = c->right;
-	}
-	free(queue);
+    free(queue);
 }
