0635afaa32cf6c4fdd38ddd1afbb142b37d3def6
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-11-29 15:35:46 +0100

    dealing with this res

--- binary_trees.h
+++ binary_trees.h
@@ -54,5 +54,6 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
 int binary_tree_is_bst(const binary_tree_t *tree);
+int is_bst_util(const binary_tree_t *node, int min, int max);
 
 #endif /* _BINARY_TREES_H_ */
